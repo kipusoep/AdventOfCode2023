cb5032016968150d5f51dd34446617f0b8e5399b
Stefan Kip <stefan.kip@wastevision.com>
2023-12-04 20:56:07 +0100

    Day 3 (in progress) Day 4

---
+++ AOC2023.3/Program.cs
@@ -0,0 +1,70 @@
+﻿var input = await File.ReadAllLinesAsync("input.txt");
+
+Exercise1();
+
+Exercise2();
+
+return;
+
+void Exercise1()
+{
+    var partNumbers = new List<int>();
+    var currentNumber = string.Empty;
+    for (var i = 0; i < input.Length; i++)
+    {
+        var line = input[i];
+        for (var j = 0; j < line.Length; j++)
+        {
+            var character = line[j];
+            if (char.IsDigit(character))
+            {
+                currentNumber = $"{currentNumber}{character}";
+            }
+
+            var isEndOfLine = j + 1 == line.Length;
+            if (!string.IsNullOrEmpty(currentNumber) && (!char.IsDigit(character) || isEndOfLine))
+            {
+                if (isEndOfLine)
+                {
+                    j++;
+                }
+
+                // Number is complete
+                var number = int.Parse(currentNumber);
+                var numberLength = currentNumber.Length;
+
+                var lineBefore = i > 0 ? input[i - 1] : string.Empty;
+                var lineAfter = i + 1 < input.Length ? input[i + 1] : string.Empty;
+                var leftSideReal = j - numberLength - 1;
+                var leftSideSafe = Math.Max(leftSideReal, 0);
+                var rightSideReal = j + 1;
+                var rightSideSafe = Math.Min(rightSideReal, line.Length);
+
+                lineBefore = !string.IsNullOrEmpty(lineBefore) ? lineBefore[leftSideSafe..rightSideSafe] : string.Empty;
+                lineAfter = !string.IsNullOrEmpty(lineAfter) ? lineAfter[leftSideSafe..rightSideSafe] : string.Empty;
+                var charBefore = leftSideReal >= 0 ? line.Substring(leftSideReal, 1) : string.Empty;
+                var charAfter = rightSideReal - 1 < line.Length ? line.Substring(rightSideReal - 1, 1) : string.Empty;
+
+                var allAdjacents = lineBefore + lineAfter + charBefore + charAfter;
+                if (allAdjacents.Any(x => x != '.' && !char.IsDigit(x)))
+                {
+                    Console.WriteLine("Part number: {0}.", number);
+                    partNumbers.Add(number);
+                }
+                else
+                {
+                    //Console.WriteLine("Normal number: {0}.", number);
+                }
+
+                currentNumber = string.Empty;
+            }
+        }
+    }
+
+    Console.WriteLine("The sum of all part numbers is: {0}.", partNumbers.Sum());
+    Console.WriteLine($"[{string.Join(", ", partNumbers)}]");
+}
+
+void Exercise2()
+{
+}
---
+++ AOC2023.4/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Text.RegularExpressions;
+
+var input = await File.ReadAllLinesAsync("input.txt");
+
+var cards = input.Select(line => new Card(line)).ToList();
+
+Exercise1();
+
+Exercise2();
+
+return;
+
+void Exercise1()
+{
+    Console.WriteLine("Total points: {0}.", cards.Sum(x => x.Points));
+}
+
+void Exercise2()
+{
+    var cardInstances = cards.Select(x => 1).ToArray();
+    for (var i = 0; i < cards.Count; i++)
+    {
+        var card = new Card(input[i]);
+        for (var j = i + 1; j <= i + card.Matches; j++)
+        {
+            cardInstances[j] += cardInstances[i];
+        }
+    }
+
+    Console.WriteLine("Total cards: {0}.", cardInstances.Sum());
+}
+
+public class Card
+{
+    public Card(string line)
+    {
+        Id = int.Parse(Regex.Match(line, @"^Card\s+(\d+):").Groups[1].Value);
+        var rest = line[(line.IndexOf(':') + 1)..];
+        var winningNumbers = rest[..rest.IndexOf('|')];
+        WinningNumbers = winningNumbers.Split(' ').Select(x => x.Trim()).Where(x => !string.IsNullOrWhiteSpace(x)).Select(int.Parse).ToList();
+        var myNumbers = rest[(rest.IndexOf('|') + 1)..];
+        MyNumbers = myNumbers.Split(' ').Select(x => x.Trim()).Where(x => !string.IsNullOrWhiteSpace(x)).Select(int.Parse).ToList();
+    }
+
+    public int Id { get; set; }
+    public List<int> WinningNumbers { get; set; }
+    public List<int> MyNumbers { get; set; }
+    public int Matches => MyNumbers.Intersect(WinningNumbers).Count();
+    public int Points => DoubleValueXTimes(Matches);
+
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return Id.ToString();
+    }
+
+    private static int DoubleValueXTimes(int times)
+    {
+        if (times == 0)
+        {
+            return 0;
+        }
+
+        var total = 1;
+        for (var i = 1; i < times; i++)
+        {
+            total *= 2;
+        }
+        return total;
+    }
+}
